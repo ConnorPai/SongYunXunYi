eb42da503ab9882970f3aefa14bd686a21ba2929
ConnorPai <165562309+ConnorPai@users.noreply.github.com>
2025-01-10 22:48:32 +0800

    Add files via upload

---
+++ index-scripts.js
@@ -0,0 +1,56 @@
+let isOn = true;
+
+function toggleImage() {
+    const button = document.getElementById('toggleButton');
+    const music = document.getElementById('backgroundMusic');
+
+    if (isOn) {
+        // Pause the music and change the button image to 'off'
+        music.pause();
+        button.src = 'offvolume.png'; // Change to your off image path
+        isOn = false;
+    } else {
+        // Play the music and change the button image to 'on'
+        music.play();
+        button.src = 'onvolume.png'; // Change back to on image path
+        isOn = true;
+    }
+}
+
+// Automatically play music when the page loads (if supported by the browser)
+window.addEventListener('load', function() {
+    const music = document.getElementById('backgroundMusic');
+    music.play().catch(function(error) {
+        console.log("Automatic playback failed. User interaction required.");
+    });
+});
+function button2Clicked() { 
+    alert('Button 2 clicked!');
+}
+
+// Add event listeners for each image to redirect to different URLs when clicked
+document.getElementById('manfabric').addEventListener('click', function() {
+    window.location.href = 'fabric.html'; // Replace with actual URL
+});
+
+document.getElementById('manfortune').addEventListener('click', function() {
+    window.location.href = 'fortune.html'; // Replace with actual URL
+});
+
+document.getElementById('manbaozi').addEventListener('click', function() {
+    window.location.href = 'baozi.html'; // Replace with actual URL
+});
+
+document.getElementById('manporcelain').addEventListener('click', function() {
+    window.location.href = 'porcelain.html'; // Replace with actual URL
+});
+
+
+
+// index-scripts.js
+
+function goToWebpage() {
+    window.location.href = "choose.html"; // 替换为目标网页的实际URL
+}
+
+// 如果您选择在HTML中通过onclick属性直接调用此函数，那么这里不需要额外的事件监听器。
